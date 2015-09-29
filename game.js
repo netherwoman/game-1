2aa0fed6701f8a928e1446ad0e2873168aeb0ae7
SkilStak Coding Arts <learn@skilstak.com>
2015-09-29 18:14:43 -0400

    save

---
+++ game.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() { }
+
+function render() { }
